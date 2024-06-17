e7ad5a47ab5ced3d86631a214622ee1d560c5e53
kramachandrashenoy <kramachandrashenoy20@gmail.com>
2024-06-17 12:35:02 +0530

    added gitignore and modified app.py (removed waitress)

--- app.py
+++ app.py
@@ -1,5 +1,26 @@
+# from flask import Flask, render_template, request
+# import os
+
+# app = Flask(__name__)
+
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
+
+# @app.route('/calculate', methods=['POST'])
+# def calculate():
+#     weight = float(request.form['weight'])
+#     height = float(request.form['height']) / 100
+#     bmi = weight / (height * height)
+#     return render_template('result.html', bmi=bmi)
+
+# if __name__ == '__main__':
+#     if os.name == 'nt':  # Checks if the operating system is Windows
+#         from waitress import serve
+#         serve(app, host='0.0.0.0', port=8080)
+#     else:
+#         app.run(debug=True)
 from flask import Flask, render_template, request
-import os
 
 app = Flask(__name__)
 
@@ -15,8 +36,4 @@ def calculate():
     return render_template('result.html', bmi=bmi)
 
 if __name__ == '__main__':
-    if os.name == 'nt':  # Checks if the operating system is Windows
-        from waitress import serve
-        serve(app, host='0.0.0.0', port=8080)
-    else:
-        app.run(debug=True)
+    app.run(debug=True)
