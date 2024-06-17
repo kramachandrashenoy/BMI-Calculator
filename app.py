956fc32fe48a8f9ed9b6098472208a3635bb05ac
kramachandrashenoy <kramachandrashenoy20@gmail.com>
2024-06-17 12:12:18 +0530

    second commit

--- app.py
+++ app.py
@@ -1,17 +1,22 @@
 from flask import Flask, render_template, request
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
 
 @app.route('/calculate', methods=['POST'])
 def calculate():
-    height = float(request.form['height'])
     weight = float(request.form['weight'])
-    bmi = weight / (height ** 2)
+    height = float(request.form['height']) / 100
+    bmi = weight / (height * height)
     return render_template('result.html', bmi=bmi)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    if os.name == 'nt':  # Checks if the operating system is Windows
+        from waitress import serve
+        serve(app, host='0.0.0.0', port=8080)
+    else:
+        app.run(debug=True)
